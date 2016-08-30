536f0860c2c8165d9c377f5f6295cf9201af417f
Deepak Grover <deepakgrover333@gmail.com>
2016-08-30 21:08:52 +0530

    add and setup redux-thunk for async

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
+import { loadCourses } from './actions/courseActions';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
